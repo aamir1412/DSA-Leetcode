d8fe5284b2f4e7b74d147b8aed7ce01b17d49661
aamir1412 <102943699+aamir1412@users.noreply.github.com>
2023-02-27 16:07:47 -0500

    Time: 263 ms (96.77%), Space: 15.4 MB (98.94%) - LeetHub

--- 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.py
+++ 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.py
@@ -1,35 +1,33 @@
 class Solution:
     def pacificAtlantic(self, heights: List[List[int]]) -> List[List[int]]:
-        ROWS, COLS = len(heights), len(heights[0])
         pac, atl = set(), set()
-        move = [(1,0), (0,1), (-1,0), (0,-1)]
+        ROWS, COLS = len(heights), len(heights[0])
+        moves = [(1,0), (0,1), (-1,0), (0,-1)]
         
-        def dfs(x, y, prvht, visit):
-            if ((x < 0 or x >= ROWS) or
-                (y < 0 or y >= COLS) or 
-                ((x, y) in visit or heights[x][y] < prvht)):
+        def dfs(x, y, prevht, seen):
+            if (x < 0 or x >= ROWS or y < 0 or y >= COLS or 
+                (x, y) in seen or prevht > heights[x][y]):
                 return
-            prvht = heights[x][y]
-            visit.add((x,y))
-            for (r, c) in move:
-                r += x
-                c += y
-                dfs(r, c, prvht, visit)
+            prevht = heights[x][y]
+            seen.add((x, y))
+            for r, c in moves:
+                r, c = (r + x), (c + y)
+                dfs(r, c, prevht, seen)
                 
+        for r in range(ROWS):
+            dfs(r, 0, 0, pac)
+            dfs(r, COLS-1, 0, atl)             
+            
         for c in range(COLS):
             dfs(0, c, 0, pac)
             dfs(ROWS-1, c, 0, atl)
-        
-        for r in range(ROWS):
-            dfs(r, 0, 0, pac)
-            dfs(r, COLS-1, 0, atl)
-        
+                
         res = []
-        for x in pac:
-            if x in atl:
-                res.append(x)
-        
-        res.sort()        
+        for cell in pac:
+            if cell in atl:
+                res.append(cell)
         return res
+            
         
+                
                 
